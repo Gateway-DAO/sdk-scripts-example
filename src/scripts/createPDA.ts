c5fba6d12c3c80e34fe91594e82ee2a95d3d9999
Saviour1001 <harshghodkar@gmail.com>
2024-02-20 20:20:17 +0530

    nit

--- src/scripts/createPDA.ts
+++ src/scripts/createPDA.ts
@@ -2,23 +2,19 @@ import { gatewayInstance } from "../prepareInstance";
 import { UserIdentifierType } from "@gateway-dao/sdk";
 
 export async function createPDA() {
-  try {
-    let obj = {
-      dataModelId: "9f27397e-27f2-4c30-b1b7-829371de4df5",
-      description: "Description of the PDA",
-      title: "Favorite Person on Crypto Twitter",
-      claim: {
-        handleName: "@gateway_xyz",
-        favoritePosts: ["awesome"],
-      },
-      owner: {
-        type: UserIdentifierType.GATEWAY_ID,
-        value: "saviour1002",
-      },
-    };
-    const { createPDA } = await gatewayInstance.pda.createPDA(obj);
-    console.log(createPDA);
-  } catch (error) {
-    console.log(error); // Can log it for debugging
-  }
+  let obj = {
+    dataModelId: "9f27397e-27f2-4c30-b1b7-829371de4df5",
+    description: "Description of the PDA",
+    title: "Favorite Person on Crypto Twitter",
+    claim: {
+      handleName: "@gateway_xyz",
+      favoritePosts: ["awesome"],
+    },
+    owner: {
+      type: UserIdentifierType.GATEWAY_ID,
+      value: "saviour1002",
+    },
+  };
+  const { createPDA } = await gatewayInstance.pda.createPDA(obj);
+  console.log(createPDA);
 }
