dc184ce7a4c1a3c7383b99c08ac9404c09a7ecff
Saviour1001 <harshghodkar@gmail.com>
2024-05-07 21:45:21 +1000

    nit

--- src/v2/scripts/index.ts
+++ src/v2/scripts/index.ts
@@ -1,5 +1,5 @@
 export * from "./PDA";
 export * from "./DataRequestTemplate";
-export * from "./createDataRequestTemplate";
+export * from "./createDataRequest";
 export * from "./transaction";
 export * from "./createDataModel";
